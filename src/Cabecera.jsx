7aebb13566f16a5f45e7d16cb26d332f76979309
KenzoMoreno <renzo202930@gmail.com>
2020-10-04 16:29:51 -0500

    probando commit

--- src/Cabecera.jsx
+++ src/Cabecera.jsx
@@ -6,7 +6,7 @@ const Cabecera  = () => (
     <div class="lg-cols-4 lg-x-2">
       <img class="main-banner__img" src="https://img.blogs.es/smartcitylab/wp-content/uploads/2020/01/ciudades-con-empleo-y-trabajo-densidad-poblacional-1920x960.jpg" alt="mirame"/>
       <div class="main-banner__data s-center">
-        <p class="t2 s-mb-0">Título del banner</p>
+        <p class="t2 s-mb-0">Título del kenzo</p>
         <p> Subtítulo del banner</p>
         <a href="https://web.whatsapp.com/" class="button">Botón del banner</a>
       </div>
