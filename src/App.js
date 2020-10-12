fe078c85f5fb900a2bc609effeb3653b39529e3d
KenzoMoreno <renzo202930@gmail.com>
2020-10-11 21:28:50 -0500

    tercer comentario

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./styles/syles.scss";
+import "./styles/estilos.css";
 import Curso from './Curso';
 import Cabecera from './Cabecera'
 
@@ -11,17 +12,20 @@ const cursos = [
       "price": 30,
       "profesor": "beto quiroga1"
 
-  }, {
+  }, 
+  {
     "title": "css",
     "image": "https://soydigital.com/wp-content/uploads/2020/05/CSS3.jpg",
     "price": 30,
     "profesor": "beto quiroga 2"
-  }, {
+  }, 
+  {
     "title": "java script",
       "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png",
       "price":40,
       "profesor": "beto quiroga 3"
-  },{
+  },
+  {
     "title": "html",
       "image": "https://p.kindpng.com/picc/s/23-237381_java-html-language-logo-png-transparent-png.png",
       "price": 50,
