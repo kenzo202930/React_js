6401d979c21c5a6015f8eec9b0d2aa0d5a8be34d
KenzoMoreno <renzo202930@gmail.com>
2020-10-11 20:42:46 -0500

    segunda suida

--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
+import PropTypes  from 'prop-types';
 
 
-const curso  = {
-    "title": "React componentes",
-    "image": "https://img.blogs.es/smartcitylab/wp-content/uploads/2020/01/ciudades-con-empleo-y-trabajo-densidad-poblacional-1920x960.jpg",
-    "price": "100uds"
-}
 
-const Curso = () => (
+const Curso = ({title, image, price, profesor})  => (
     <article className="card">
         <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-        <img src={curso.image} alt={curso.tile} />      
+        <img src={image} alt={title} />      
         </div>
         <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-            <h3 className="center">{curso.title}</h3>   
+            <h3 className="center">{title}</h3>   
             <div className="s-main-center">
-                <a className="button--ghost-alert button--tiny" href="#">{ `$ ${curso.price}`}</a>
+              {profesor}    
+            </div>
+            <div className="s-main-center">
+                <a className="button--ghost-alert button--tiny" href="#">{ `$${price} USD`}</a>
+             
             </div>
         </div>
     </article>  
 )
-export default Curso;
+
+Curso.prototype = {
+    titulo: PropTypes.string,
+    image: PropTypes.string,
+    price: PropTypes.number,
+    profesor: PropTypes.string
+}
+
+Curso.defaultProps = {
+    titulo: "no se encontro data",
+    image: "https://dmm40cf0lyret.cloudfront.net/wp-content/uploads/2018/12/Desarrollo-de-aplicaciones-con-React-Native.jpg",
+    price: "--",
+    profesor: "NO HAY PROFE"
+}
+
+export default Curso; 
